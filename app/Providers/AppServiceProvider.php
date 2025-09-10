3fa75933a3866ad301399beb4d751fe33d9f82c1
torgodly <torgodly@gmail.com>
2025-09-11 01:30:32 +0200

    add image handling to posts API endpoints and remove media attribute

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //force https scheme in production
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
